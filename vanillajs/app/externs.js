e0a4c4bc33718a430fdb946015d2faeded575a14
Sam Saccone <samccone@gmail.com>
2017-04-08 16:04:39 -0700

    Add externs to support closure compiler

---
+++ vanillajs/app/externs.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {{kids: Array<number>, idx: number, url: string, score: number, by:
+ * string, id: number, descendants: number}}
+ */
+var Story;
+
+/**
+ * @typedef {{deleted: boolean, id: number, parent: number, time: number, type:
+ * string}}
+ */
+var StoryComment;
