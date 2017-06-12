de1715790376d6f7637cecadda7a8e0486c38bce
Sam Saccone <samccone@gmail.com>
2017-06-11 17:29:42 -0700

    level up service worker

--- vanillajs/app/sw.js
+++ vanillajs/app/sw.js
@@ -1,46 +1,41 @@
 var VERSION = '0.1';
-
-this.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(VERSION).then(cache => {
-    return cache.addAll([
-      '/', '/top', '/newest', '/ask', '/jobs', '/sw.js', '/app.css', '/cc.js'
-    ]);
-  }))
-});
-
-this.addEventListener('fetch', function(e) {
-  var tryInCachesFirst = caches.open(VERSION).then(cache => {
-    return cache.match(e.request).then(response => {
-      if (!response) {
-        return handleNoCacheMatch(e);
-      }
-
-      // Update cache record in the background
-      updateRequestCache(e);
-      return response
-    });
-  });
-
-  e.respondWith(tryInCachesFirst);
-});
-
-this.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(keys => {
-    return Promise.all(keys.map(key => {
-      if (key !== VERSION) return caches.delete(key);
-    }));
-  }));
-});
-
-function updateRequestCache(e) {
-  return fetch(e.request).then(res => {
-    return caches.open(VERSION).then(cache => {
-      cache.put(e.request, res.clone());
-      return res;
-    });
-  });
+this.addEventListener('install', e => e.waitUntil(swInstall()));
+this.addEventListener('activate', e => e.waitUntil(swActivate()));
+this.addEventListener('fetch', e => e.respondWith(swFetch(e)));
+async function swInstall() {
+  const rs = await fetch('./bundle.txt');
+  const body = await rs.text();
+  const cache = await caches.open(VERSION);
+  await cache.addAll(body.trim().split('\n'));
+  await this.skipWaiting();
 }
-
-function handleNoCacheMatch(e) {
-  return updateRequestCache(e);
+async function swActivate() {
+  const keys = await caches.keys();
+  let deletes = [];
+  for (var key of keys) {
+    if (key !== VERSION) deletes.push(caches.delete(key));
+  }
+  await Promise.all(deletes);
+  await this.clients.claim();
+}
+async function swFetch(e) {
+  var networkFetch = fetchFromNetworkAndCache(e);
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(e.request);
+  if (response) return response;
+  return networkFetch;
+}
+async function fetchFromNetworkAndCache(e) {
+  if (new URL(e.request.url).origin !== location.origin) {
+    return new Response(new Blob(), { status: 404, statusText: 'Not found' });
+  }
+  const res = await fetch(e.request);
+  if (!res.url) {
+    // foreign requests will be res.type === 'opaque' and missing a url
+    return res;
+  }
+  const cache = await caches.open(VERSION);
+  // TODO: figure out if the content is new and therefore the page needs a reload.
+  cache.put(e.request, res.clone());
+  return res;
 }
