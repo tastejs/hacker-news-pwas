426c5dd150348152c52d1b51eaaf63b535bbddb1
Sam Saccone <samccone@gmail.com>
2017-06-13 18:55:30 -0700

    Add deploy script

--- vanillajs/app/compile.sh
+++
@@ -1,1 +0,0 @@
-java -jar "$CC_PATH" --externs=externs.js --js=app.js --language_in=ECMASCRIPT6 --language_out=ECMASCRIPT5 --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills=false > cc.js
