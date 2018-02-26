b34e4208f58d86f35f58e0e95b58a42788e2bbb3
David East <deast@google.com>
2018-02-26 11:31:36 -0700

    Remove extra files

--- api/src/server.ts
+++
@@ -1,5 +0,0 @@
-import * as hnpwa from 'hnpwa-api';
-
-export const startServer = (port = 3002, startMessage = 'Listening...') => {
-  hnpwa.app('blah').listen(port, () => console.log(startMessage));
-};
